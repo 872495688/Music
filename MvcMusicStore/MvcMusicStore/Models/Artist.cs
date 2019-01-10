e02bdd95a6fa78f9468d550c9641166ede4ad596
872495688 <872495688@qq.com>
2019-01-10 15:09:04 +0800

    1

--- MvcMusicStore/MvcMusicStore/Models/Artist.cs
+++ MvcMusicStore/MvcMusicStore/Models/Artist.cs
@@ -7,14 +7,7 @@ namespace MvcMusicStore.Models
 {
     public class Artist
     {
-        //唱片集类
-        public int AlbumId { get; set; }
-        public int GenreId { get; set; }
-        public int ArtistId { get; set; }
-        public string Title { get; set; }
-        public decimal Price { get; set; }
-        public string AlbumArtUrl { get; set; }
-        public Genre Genre { get; set; }
-        //public Artist Artist { get; set; }
+        
+       
     }
 }
---
+++ MvcMusicStore/MvcMusicStore/Models/MusicStoreEntities.cs
@@ -0,0 +1,16 @@
+﻿namespace MvcMusicStore.Models
+{
+    public class MusicStoreEntities : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+
+    public class DbSet<T>
+    {
+    }
+
+    public class DbContext
+    {
+    }
+}
